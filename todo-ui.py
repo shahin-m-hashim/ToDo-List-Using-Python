fbced97da0b8bdbfa7594390cb20fdd2fd1d35c0
Shahin M Hashim <98640366+shahin-m-hashim@users.noreply.github.com>
2023-01-31 21:46:16 +0530

    Update todo-ui.py

--- todo-ui.py
+++ todo-ui.py
@@ -26,10 +26,42 @@ class RightFrame(ttk.Frame):
         self.columnconfigure(0, weight=1)
 
     def add_todo(self):
-        pass
+        add_todo_gui = tk.Toplevel(self)
+        AddTodoGUI(add_todo_gui)
 
     def delete_todo(self):
         pass
+    
+class AddTodoGUI:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Add Todo")
+        self.master.geometry("400x230")
+        self.master.resizable(False, False)
+        self.master.columnconfigure(0, weight=1, pad=1)
+        self.master.columnconfigure(1, weight=1, pad=1)
+        self.master.rowconfigure(0, pad=3)
+        self.master.rowconfigure(1, pad=3)
+        self.master.rowconfigure(2, pad=3)
+        self.master.rowconfigure(3, pad=3)
+
+        todo_name_label = ttk.Label(self.master, text="ToDo Name:", width=25)
+        todo_name_label.grid(row=0, column=0, sticky="w",pady=5)
+        self.todo_name = ttk.Entry(self.master, width=40)
+        self.todo_name.grid(row=0, column=1,padx=5)
+
+        todo_description_label = ttk.Label(self.master, text="ToDo Description:", width=25)
+        todo_description_label.grid(row=1, column=0, sticky="w",pady=5)
+        self.todo_description = tk.Text(self.master, width=30, height=5)
+        self.todo_description.grid(row=1, column=1,padx=4)
+
+        due_date_label = ttk.Label(self.master, text="Due Date:", width=25)
+        due_date_label.grid(row=2, column=0, sticky="w",pady=5)
+        self.due_date = ttk.Entry(self.master, width=40,)
+        self.due_date.grid(row=2, column=1,padx=5)
+
+        self.save_button = ttk.Button(self.master, text="Save", command=self.save, width=20)
+        self.save_button.grid(row=3, columnspan=2, pady=10)
 
 class TodoListApp:
     def __init__(self, master):
@@ -37,7 +69,7 @@ class TodoListApp:
         self.master.title("Todo List")
         self.master.geometry("800x600")
         self.master.columnconfigure(0, weight=3)
-        self.master.columnconfigure(1, weight=1)  # added line
+        self.master.columnconfigure(1, weight=1)
         self.master.rowconfigure(0, weight=1)
 
         self.left_frame = LeftFrame(self.master)
